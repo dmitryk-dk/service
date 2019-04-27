84266e07dfc03fe22cf61a3ad04d9b9cbd952b58
dmitryk-dk <DAKozlov@luxoft.com>
2019-04-27 12:31:40 +0300

    update storage tests

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -11,36 +11,38 @@ type testStorageStruct struct {
 	Value  string
 	Key    string
 	Error  string
+	Result string
 }
 
 var testSetData = []testStorageStruct{
-	testStorageStruct{"set", "my_value_for_empty_key", "", "key not set"},
-	testStorageStruct{"set", "my_value_for_long_key", "my_very_long_key_10000000", "key too long"},
-	testStorageStruct{"set", "my_value_for_key_1", "my_key_1", ""},
-	testStorageStruct{"set", "", "my_key_2", ""},
+	testStorageStruct{"set", "my_value_for_empty_key", "", "key not set", ""},
+	testStorageStruct{"set", "my_value_for_long_key", "my_very_long_key_10000000", "key too long", ""},
+	testStorageStruct{"set", "my_value_for_key_1", "my_key_1", "", "success"},
+	testStorageStruct{"set", "", "my_key_2", "", "success"},
+	testStorageStruct{"set", "my_value_for_key_3", "my_key_3", "", "success"},
 }
 
 var testGetData = []testStorageStruct{
-	testStorageStruct{"set", "my_value_for_empty_key", "", "key not set"},
-	testStorageStruct{"set", "my_value_for_key_1", "my_key_1", "my_value_for_key_1"},
-	testStorageStruct{"set", "", "my_key_2", "value not found"},
-	testStorageStruct{"set", "", "qwertyuiopasdfghaa", "key too long"},
-	testStorageStruct{"set", "some_value", "qwertyuiopasdfghaa", "key too long"},
+	testStorageStruct{"get", "my_value_for_empty_key", "", "key not set", ""},
+	testStorageStruct{"get", "my_value_for_key_1", "my_key_1", "my_value_for_key_1", ""},
+	testStorageStruct{"get", "", "my_key_2", "", ""},
+	testStorageStruct{"get", "", "qwertyuiopasdfghaa", "key too long", ""},
+	testStorageStruct{"get", "some_value", "qwertyuiopasdfghaa", "key too long", ""},
 }
 
 var testExistData = []testStorageStruct{
-	testStorageStruct{"set", "my_value_for_empty_key", "", "key not set"},
-	testStorageStruct{"set", "my_value_for_key_1", "my_key_1", ""},
-	testStorageStruct{"set", "", "my_key_2", "value not found"},
-	testStorageStruct{"set", "", "qwertyuiopasdfghaa", "key too long"},
-	//testStorageStruct{"set", "some_value", "qwertyuiopasdfghaa", "key too long"},
+	testStorageStruct{"exist", "my_value_for_empty_key", "", "key not set", ""},
+	testStorageStruct{"exist", "my_value_for_key_1", "my_key_1", "", "success"},
+	testStorageStruct{"exist", "", "my_key_2", "", "success"},
+	testStorageStruct{"exist", "", "qwertyuiopasdfghaa", "key too long", ""},
+	testStorageStruct{"exist", "some_value", "qwertyuiopasdfghaa", "key too long", ""},
 }
 
 var testDeleteData = []testStorageStruct{
-	testStorageStruct{"set", "my_value_for_empty_key", "", "key not set"},
-	testStorageStruct{"set", "my_value_for_long_key", "my_very_long_key_10000000", "key too long"},
-	testStorageStruct{"set", "my_value_for_key_1", "my_key_1", ""},
-	testStorageStruct{"set", "", "my_key_2", "value not found"},
+	testStorageStruct{"set", "my_value_for_empty_key", "", "key not set", ""},
+	testStorageStruct{"set", "my_value_for_long_key", "my_very_long_key_10000000", "key too long", ""},
+	testStorageStruct{"set", "my_value_for_key_1", "my_key_1", "", "success"},
+	testStorageStruct{"set", "", "my_key_2", "value not found", ""},
 }
 
 func ErrorContains(out string, want string) bool {
@@ -60,11 +62,11 @@ func TestSet(t *testing.T) {
 			Value:  data.Value,
 			Key:    data.Key,
 		}
-		errStr := store.Set()
-		if errStr != data.Error {
-			t.Errorf("errStr '%v'; data.Error '%v';", errStr, data.Error)
+		storage := store.Set()
+		if storage.Error != data.Error {
+			t.Errorf("errStr '%v'; data.Error '%v';", storage.Error, data.Error)
 		}
-		if errStr == "" {
+		if storage.Error == "" {
 			if DbStorage[data.Key] != data.Value {
 				t.Errorf("DbStorage value '%v'; test data value '%v'", DbStorage[data.Key], data.Value)
 			}
@@ -75,11 +77,6 @@ func TestSet(t *testing.T) {
 			checkSet(t, v)
 		})
 	}
-	t.Run("run test for check DBLength", func(t *testing.T) {
-		if CheckDbLenght() != 2 {
-			t.Errorf("Db length was: '%v'; want: '%d'", CheckDbLenght(), 2)
-		}
-	})
 	fmt.Printf("** TestSet - ALL PASSED (number of test cases: %d)**\n", len(testSetData))
 }
 
@@ -91,15 +88,15 @@ func TestGet(t *testing.T) {
 			Value:  data.Value,
 			Key:    data.Key,
 		}
-		_, value := store.Get()
-		if value != "" {
-			if !ErrorContains(value, data.Error) {
-				t.Errorf("%s", value)
+		storage := store.Get()
+		if storage.Error != "" {
+			if storage.Error != data.Error {
+				t.Errorf("Error in get method was '%v'; want '%v'", storage.Error, data.Error)
 			}
 		}
-		if value == "" {
-			if value != data.Value {
-				t.Errorf("Value getted from db '%v'; value from test '%v", value, data.Value)
+		if storage.Error == "" {
+			if storage.Value != data.Value {
+				t.Errorf("Value getted from db '%v'; value from test '%v", storage.Value, data.Value)
 			}
 		}
 	}
@@ -119,13 +116,13 @@ func TestExist(t *testing.T) {
 			Value:  data.Value,
 			Key:    data.Key,
 		}
-		ok, errStr := store.Exist()
-		if errStr != "" && errStr != data.Error {
-			t.Errorf("errStr '%v'; data.Error '%v'; key '%v'", errStr, data.Error, data.Key)
+		storage := store.Exist()
+		if storage.Error != "" && storage.Error != data.Error {
+			t.Errorf("Error in exist methos was '%v'; want '%v'; key '%v'", storage.Error, data.Error, data.Key)
 		}
-		if errStr == "" {
-			if !ok {
-				t.Errorf("Value not found in db check method exist '%v'", ok)
+		if storage.Error == "" {
+			if storage.Result != data.Result {
+				t.Errorf("Exist result was '%v'; want '%v'", storage.Result, data.Result)
 			}
 		}
 	}
@@ -145,14 +142,12 @@ func TestDelete(t *testing.T) {
 			Value:  data.Value,
 			Key:    data.Key,
 		}
-		_, errStr := store.Delete()
-		if errStr != "success" && errStr != data.Error {
-			t.Errorf("errStr '%v'; data.Error '%v';", errStr, data.Error)
+		storage := store.Delete()
+		if storage.Result != "success" && storage.Error != data.Error {
+			t.Errorf("Error in delete methis was '%v'; want '%v';", storage.Error, data.Error)
 		}
-		if errStr == "" {
-			if _, ok := DbStorage[data.Key]; ok {
-				t.Error("Method delete not remove data from Db")
-			}
+		if storage.Error == "" && storage.Result != "success" {
+			t.Error("Method delete not remove data from Db")
 		}
 	}
 	for _, v := range testDeleteData {
